6017f0feb252c0fce695998e1b384127548cfeb8
Victor Santos Silva <victor.s.silva@unesp.br>
2021-06-08 16:19:51 -0300

    api has been finished

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from . import views
+from api.views import Restaurants, RestaurantDetail, Recipes, RecipeDetail
 
 urlpatterns = [
+    path('restaurants/', Restaurants.as_view()),
+    path('restaurants/<str:restaurant_id>/', RestaurantDetail.as_view()),
+    path('restaurants/<str:restaurant_id>/recipes/', Recipes.as_view()),
+    path('restaurants/<str:restaurant_id>/recipes/<str:recipe_id>/', RecipeDetail.as_view())
 ]
