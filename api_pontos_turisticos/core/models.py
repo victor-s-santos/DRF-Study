cfecf9aeb1bd073937798e2744d001628c616191
Victor Santos Silva <victor.s.silva@unesp.br>
2021-06-09 15:26:22 -0300

    finishing endereco app

--- api_pontos_turisticos/core/models.py
+++ api_pontos_turisticos/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from atracoes.models import Atracao
 from comentarios.models import Comentario
 from avaliacoes.models import Avaliacao
+from enderecos.models import Endereco
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
@@ -10,7 +11,8 @@ class PontoTuristico(models.Model):
     aprovado = models.BooleanField(default=False)
     comentarios = models.ManyToManyField(Comentario)
     avaliacoes = models.ManyToManyField(Avaliacao)
-    
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.nome
 # Create your models here.
