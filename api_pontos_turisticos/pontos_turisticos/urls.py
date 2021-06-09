f94e87e1375831cd94179c8eea891866ef2776b5
Victor Santos Silva <victor.s.silva@unesp.br>
2021-06-09 17:33:48 -0300

    seting serializers and viewsets

--- api_pontos_turisticos/pontos_turisticos/urls.py
+++ api_pontos_turisticos/pontos_turisticos/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from core.api.viewsets import PontoTuristicoViewSet
+
+router = routers.DefaultRouter()
+router.register(r'pontoturistico', PontoTuristicoViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
