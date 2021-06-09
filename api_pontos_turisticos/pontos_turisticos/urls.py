3f26cbe54c191cab51c0155ae1a5bbd3492b0d42
Victor Santos Silva <victor.s.silva@unesp.br>
2021-06-09 20:19:06 -0300

    add enderecos endpoint

--- api_pontos_turisticos/pontos_turisticos/urls.py
+++ api_pontos_turisticos/pontos_turisticos/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls import include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
+from enderecos.api.viewsets import EnderecosViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
+router.register(r'enderecos', EnderecosViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
