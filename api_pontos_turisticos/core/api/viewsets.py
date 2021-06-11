33b1651aa1f34b15e08a8141fa4c57b83a0c3d20
Victor Santos Silva <victor.s.silva@unesp.br>
2021-06-11 18:57:46 -0300

    understanding the built in methods

--- api_pontos_turisticos/core/api/viewsets.py
+++ api_pontos_turisticos/core/api/viewsets.py
@@ -1,6 +1,9 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
+from rest_framework.decorators import action
+
 
 class PontoTuristicoViewSet(ModelViewSet):
     """
@@ -11,3 +14,40 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+    """
+    #subrescrevendo list
+    def list(self, request, *args, **kwargs):
+        return Response('list')
+    """
+    """
+    #subrescrevendo create
+    def create(self, request, *args, **kwargs):
+        pass
+    """
+    """
+    #subrescrevendo delete
+    def destroy(self, request, *args, **kwargs):
+        pass
+    """
+
+    """
+    #subscrevendo retrieve
+    def retrieve(self, request, *args, **kwargs):
+        pass
+    """
+
+    """
+    #subscrevendo o update
+    def update(self, request, *args, **kwargs):
+        pass
+    """
+    """
+    #subscrevendo o partial update
+    def partial_update(self, request, *args, **kwargs):
+        pass
+    """
+
+    #funcao customizada
+    """@action(methods=['GET'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass"""
