bda784349cf32fcc3a0c0ac58aacfa888ac7d330
Victor Silva <victor.s.silva@unesp.br>
2021-06-26 17:36:31 -0300

    implemented new methods

--- basic/cursos/views.py
+++ basic/cursos/views.py
@@ -3,11 +3,20 @@ from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(generics.ListCreateAPIView):
+class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 
-class AvaliacaoAPIView(generics.ListCreateAPIView):
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
